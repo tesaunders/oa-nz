10406e41c9ec8846c07c54ee7a391a0e76b05e0b
Tom Saunders <tom.saunders@auckland.ac.nz>
2025-03-12 23:16:17 +1300

    Improved json->dataframe conversion, include topic/field in query + report

--- uoa-report/query-uoa.R
+++ uoa-report/query-uoa.R
@@ -11,7 +11,7 @@ pub_years <- (as.numeric(format(Sys.Date(), "%Y"))-10):(as.numeric(format(Sys.Da
 
 # Build API request (using email address stored as environment variable) --
 
-req_citation <-
+req_uoa <-
   request("https://api.openalex.org/") |> 
   req_url_path_append("works") |> 
   req_url_query(filter = paste0("is_paratext:false,is_retracted:false,type:article|book|book-chapter,",
@@ -19,16 +19,17 @@ req_citation <-
                                 paste0(pub_years, collapse = "|"), 
                                 ",", 
                                 "authorships.institutions.lineage:i154130895")) |> 
-  req_url_query(select = c("id", "publication_year", "type", "cited_by_count", "open_access"), .multi = "comma") |>
+  req_url_query(select = c("id", "publication_year", "type", "cited_by_count", "open_access",
+                           "fwci", "primary_topic"), .multi = "comma") |>
   req_url_query(mailto = Sys.getenv("EMAIL_ADDRESS")) |> 
   req_url_query("per-page" = "200") |>
   req_url_query(cursor = "*")
 
 # Make request, retrieving publication data with cursor-based pagination --
 
-resp_citation <- 
+resp_uoa <- 
   req_perform_iterative(
-    req = req_citation,
+    req = req_uoa,
     next_req = iterate_with_cursor(
       param_name = "cursor",
       resp_param_value = \(resp) resp_body_json(resp)$meta$next_cursor
@@ -38,15 +39,22 @@ resp_citation <-
 
 # Process the json into a dataframe ---------------------------------------
 
-data_citation <-
-  resp_citation |> 
+data_uoa <-
+  resp_uoa |> 
   map(resp_body_json) |>
   map("results") |>
   list_flatten() |> 
-  map(bind_cols) |> 
-  bind_rows() |> 
-  select(-oa_url, -any_repository_has_fulltext)
-
+  map(~ tibble(
+    id = .x$id,
+    publication_year = .x$publication_year,
+    type = .x$type,
+    cited_by_count = .x$cited_by_count,
+    is_oa = .x$open_access$is_oa,
+    oa_status = .x$open_access$oa_status,
+    field = .x$primary_topic$field$display_name
+  )) |>
+  bind_rows()
+  
 # Export data as csv ------------------------------------------------------
 
-write.csv(data_citation, "uoa-report/uoa-citations.csv", row.names = FALSE)
+write.csv(data_uoa, "uoa-report/uoa-data.csv", row.names = FALSE)
